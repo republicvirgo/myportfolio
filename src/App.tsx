029dcd0e7c695d218330e190ec14013bea37cd94
dony putra <republicirgo@gmail.com>
2023-02-04 01:50:18 +0700

    buat navigasi

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import logo from './assets/logo.svg';
 import styles from './App.module.scss';
 
 function App() {
     return (
         <div className={styles.App}>
-            <header className={styles['App-header']}>
-                <img src={logo} className={styles['App-logo']} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={styles['App-link']}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <nav>
+                <a href="/home">Home</a> | <a href="/projects">Projects</a> |{' '}
+                <a href="/about">About</a> | <a href="/contact">Contact Us</a>
+            </nav>
         </div>
     );
 }
